89d9712f1f542f012a44c87a30889420618cf9f8
Goncharov Cyril <Krio05@yandex.ua>
2017-07-03 21:42:28 +0300

    Добавил получение мест передвижения курсора и захвата координат

--- Program/Program/Form1.Designer.cs
+++ Program/Program/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Program
 {
-    partial class Form1
+    partial class MainWindow
     {
         /// <summary>
         /// Требуется переменная конструктора.
@@ -28,12 +28,65 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainWindow));
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.GetAreaBtn = new System.Windows.Forms.Button();
+            this.StartMoveBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.richTextBox1.Location = new System.Drawing.Point(22, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(596, 137);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = resources.GetString("richTextBox1.Text");
+            // 
+            // GetAreaBtn
+            // 
+            this.GetAreaBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.GetAreaBtn.Location = new System.Drawing.Point(22, 248);
+            this.GetAreaBtn.Name = "GetAreaBtn";
+            this.GetAreaBtn.Size = new System.Drawing.Size(219, 42);
+            this.GetAreaBtn.TabIndex = 1;
+            this.GetAreaBtn.Text = "Выбрать точки";
+            this.GetAreaBtn.UseVisualStyleBackColor = true;
+            this.GetAreaBtn.Click += new System.EventHandler(this.GetPlaceBtn_Click);
+            this.GetAreaBtn.KeyDown += new System.Windows.Forms.KeyEventHandler(this.WindowKey_Down);
+            // 
+            // StartMoveBtn
+            // 
+            this.StartMoveBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.StartMoveBtn.Location = new System.Drawing.Point(399, 248);
+            this.StartMoveBtn.Name = "StartMoveBtn";
+            this.StartMoveBtn.Size = new System.Drawing.Size(219, 42);
+            this.StartMoveBtn.TabIndex = 2;
+            this.StartMoveBtn.Text = "Начать захват";
+            this.StartMoveBtn.UseVisualStyleBackColor = true;
+            this.StartMoveBtn.KeyDown += new System.Windows.Forms.KeyEventHandler(this.WindowKey_Down);
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(639, 413);
+            this.Controls.Add(this.StartMoveBtn);
+            this.Controls.Add(this.GetAreaBtn);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "MainWindow";
+            this.ShowIcon = false;
+            this.Text = "GCoordinates";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.WindowKey_Down);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button GetAreaBtn;
+        private System.Windows.Forms.Button StartMoveBtn;
     }
 }
 
--- Program/Program/Form1.cs
+++ Program/Program/Form1.cs
@@ -10,11 +10,121 @@ using System.Windows.Forms;
 
 namespace Program
 {
-    public partial class Form1 : Form
+    public partial class MainWindow : Form
     {
-        public Form1()
+        public MainWindow()
         {
             InitializeComponent();
         }
+
+        /**
+         * Флаг начала определения области
+         * 
+         * Используется для начала определения
+         * мест передвижения курсора и захвата координат.
+         */
+        private bool IsGetArea = false;
+
+        /**
+         * Стадия определения области
+         * 
+         * Используется для определения того, кикие точки
+         * мест уже выбраны.
+         */
+        private int GetAreaState = 0;
+
+        /**
+         * Первый угол передвижения курсора
+         * 
+         * Используется для определения места передвижения
+         * курсора. Верхний левый угол прямоугольника
+         * области, в которой передвигается курсор.
+         * Выражено двумя переменными X и Y.
+         */
+        private int AreaCursorX1, AreaCursorY1;
+
+        /**
+         * Второй угол передвижения курсора
+         * 
+         * Используется для определения места передвижения
+         * курсора. Нижний правый угол прямоугольника
+         * области, в которой передвигается курсор.
+         * Выражено двумя переменными X и Y.
+         */
+        private int AreaCursorX2, AreaCursorY2;
+
+        /**
+         * Первый угол захвата координат
+         * 
+         * Используется для определения места захвата координат. 
+         * Верхний левый угол прямоугольника
+         * области, в которой идет захват.
+         * Выражено двумя переменными X и Y.
+         */
+        private int AreaCoordinatesX1, AreaCoordinatesY1;
+
+        /**
+         * Второй угол захвата координат
+         * 
+         * Используется для определения места захвата координат. 
+         * Правый нижний угол прямоугольника
+         * области, в которой идет захват.
+         * Выражено двумя переменными X и Y.
+         */
+        private int AreaCoordinatesX2, AreaCoordinatesY2;
+
+
+
+        /**
+         * Событие нажатия кнопки "Выбрать точки"
+         * 
+         * При нажатии окно становиться полупрозрачным.
+         * Флаг для перехвата события нажатия на клавишу
+         * пристваивается true
+         */
+        private void GetPlaceBtn_Click(object sender, EventArgs e)
+        {
+            IsGetArea = true;
+            this.Opacity = 0.8;             //this используется для обращение к объекту формы. Opacity-прозрачность
+            GetAreaState = 1;
+        }
+
+        private void WindowKey_Down(object sender, KeyEventArgs e)
+        {
+            if(IsGetArea && e.KeyCode==Keys.C)
+            {
+                switch(GetAreaState)
+                {
+                    case 1:
+                        AreaCursorX1 = Cursor.Position.X;
+                        AreaCursorY1 = Cursor.Position.Y;
+                        GetAreaState = 2;
+                        break;
+                    case 2:
+                        AreaCursorX2 = Cursor.Position.X;
+                        AreaCursorY2 = Cursor.Position.Y;
+                        GetAreaState = 3;
+                        break;
+                    case 3:
+                        AreaCoordinatesX1 = Cursor.Position.X;
+                        AreaCoordinatesY1 = Cursor.Position.Y;
+                        GetAreaState = 4;
+                        break;
+                    case 4:
+                        AreaCoordinatesX2 = Cursor.Position.X;
+                        AreaCoordinatesY2 = Cursor.Position.Y;
+                        GetAreaState = 0;
+                        IsGetArea = false;
+                        this.Opacity = 1;
+                        break;
+                }
+            }
+        }
+
+
+
+
+
+
     }
 }
